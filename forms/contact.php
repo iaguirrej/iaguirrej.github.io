4c55de5285298ab41fcba1b3804f3645167ae9be
Imer Aguirre <imer.aguirre.87@gmail.com>
2023-07-22 17:18:36 -0500

    update

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'imer.aguirre.87@hotmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -30,8 +30,8 @@
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
-  );
-  */
+  );*/
+
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
